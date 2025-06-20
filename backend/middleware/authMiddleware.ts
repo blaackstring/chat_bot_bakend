a539a9b6ed235dff4922561da823d233769626f6
shahan <johndoe@email.com>
2025-06-20 10:10:59 +0530

    authmiddleware updated

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -16,8 +16,14 @@ export const authMiddleware = (req: AuthRequest, res: Response, next: NextFuncti
       res.status(401).send({ success: false, message: 'Authentication required' });
       return;
     }
+const secret = process.env.SECKEY;
 
-    const decoded = jwt.verify(token, JSON.stringify(process.env.SECKEY));
+if (!secret) {
+  throw new Error("JWT Secret Key (SECKEY) not defined in environment");
+}
+
+const decoded = jwt.verify(token, secret); // now secret is definitely a string
+
     req.user = decoded;
     console.log(req.user+'middleware');
     
