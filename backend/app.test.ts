b322b9e887578d75bd843341f39ac1db42be37fc
shahan <johndoe@email.com>
2025-06-25 15:25:35 +0530

    feature branch cicd push

---
+++ backend/app.test.ts
@@ -0,0 +1,49 @@
+import request from 'supertest';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+dotenv.config({ path: '.env.test' });
+import app from './server'; // or './server' depending on your export
+import { describe, it } from 'node:test';
+
+describe('Basic App Routes', () => {
+//   it('should return 404 on root / route', async () => {
+//     const res = await request(app).get('/');
+//     expect(res.statusCode).toBe(404);
+//   });
+
+//   it('should return 404 on /api/v1/auth if no GET route is defined', async () => {
+//     const res = await request(app).get('/api/v1/auth');
+//     expect(res.statusCode).toBe(404); // Unless you define a GET route at this path
+//   
+
+it('Root access it retrun 404 bcoz no route handler on root / ',async()=>{
+    const res=await request(app).get('/');
+    expect(res.statusCode).toBe(404)
+});
+
+
+
+
+it('should throw error if SECKEY is missing', () => {
+  process.env.SECKEY = undefined; // simulate missing key
+
+  expect(() => {
+    jwt.sign({ id: '123' }, process.env.SECKEY!);
+  }).toThrow(); // fail hone chahiye
+});
+
+
+it('request on Auth root handler / ',async()=>{
+    const res=await request(app).get('/api/v1/auth');
+    expect(res.statusCode).toBe(404); //
+});
+
+
+});
+
+
+
+function expect(statusCode: number) {
+    throw new Error('Function not implemented.');
+}
+
--- backend/server.ts
+++ backend/server.ts
@@ -1,31 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import authroute from './routes/authRoute';
-import cookieParser from 'cookie-parser';
-import { DbConnection } from './config/Db';
-import dotenv from 'dotenv'
+  import express from 'express';
+  import cors from 'cors';
+  import authroute from './routes/authRoute';
+  import cookieParser from 'cookie-parser';
+  import { DbConnection } from './config/Db';
+  import dotenv from 'dotenv'
 
-dotenv.config()
+  dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+  const app = express();
 
-app.use(cors(
- {
-   origin:'https://zerocode-fe-assignment-tw2y.vercel.app',
-   credentials:true
- }
-));
-app.use(express.json());
-app.use(cookieParser());
-console.log(process.env.MONGO_URL)
-DbConnection()
 
-// Example Route
-app.use('/api/v1/auth', authroute);
+  app.use(cors(
+  {
+    origin:'https://zerocode-fe-assignment-tw2y.vercel.app',
+    credentials:true
+  }
+  ));
+  app.use(express.json());
+  app.use(cookieParser());
+  console.log(process.env.MONGO_URL)
+  DbConnection()
 
-// Start Server
-app.listen(PORT, () => {
-  console.log(`🚀 Server listening on http://localhost:${PORT}`);
-});
+  // Example Route
+  app.use('/api/v1/auth', authroute);
 
+  // Start Server
+export default app;
